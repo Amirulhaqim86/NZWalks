5c4fd9bfac72d9c22665b5f01249c9208d3f4c29
aqiim <amirulhaqim86@gmail.com>
2023-03-20 19:06:01 +0800

    End Of Section 3

---
+++ NZWalks/NZWalks.API/Repositories/RagionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RagionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public RagionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+           return await nZWalksDbContext.Regions.ToListAsync(); 
+        }
+    }
+}
