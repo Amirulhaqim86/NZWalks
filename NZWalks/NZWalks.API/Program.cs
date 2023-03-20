524dec5c9fdc4e25b16b9e7f61cd7d7cc0dd24b3
aqiim <amirulhaqim86@gmail.com>
2023-03-21 00:17:43 +0800

    End Of Section 4

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 
 builder.Services.AddScoped<IRegionRepository, RagionRepository>();
 
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
